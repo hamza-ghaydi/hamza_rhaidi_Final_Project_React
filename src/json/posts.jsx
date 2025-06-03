cf8a889ee1c1bffa792823a85ed1a1e4e576c6bb
hamzashiva <hamzaghaydi01@gmail.com>
2025-06-03 14:01:33 +0100

    section blog

--- src/json/posts.jsx
+++ src/json/posts.jsx
@@ -9,7 +9,7 @@ export const posts = [
     categories: ["crafts", "street style"],
     comments: 1,
     image: images.blog01, 
-    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit amet tempor magna...",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit amet tempor magna. Donec eros sem, porta eget leo et, varius eleifend mauris. Donec eu leo congue, faucibus quam eu, viverra mauris. Nulla consectetur lorem mi, at scelerisque metus hendrerit vitae. Proin vel magna vel neque porta ultricies non eget mauris. Suspendisse potenti. Aliquam faucibus scelerisque placerat. Vestibulum vel libero eu nulla varius pretium eget eu magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean dictum faucibus felis, ac vestibulum risus mollis in. Phasellus neque dolor, euismod vitae auctor eget, dignissim a felis. Etiam malesuada elit a nibh aliquam, placerat ultricies nibh dictum. Nam ut egestas velit. Pellentesque viverra tincidunt tellus. Etiam cursus, ligula id vehicula cursus, turpis mauris facilisis massa, eget tincidunt est purus et odio. Nam quis luctus libero, non posuere velit. Ut eu varius diam, eu euismod elit. Donec efficitur, neque eu consectetur consectetur, dui sem consectetur felis, vitae rutrum risus urna vel arcu. Aliquam semper ullamcorper laoreet. Sed arcu lectus, fermentum imperdiet purus eu, ornare ornare libero.",
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ export const posts = [
     categories: ["gifts", "lifestyle"],
     comments: 3,
     image: images.blog02,
-    excerpt: "Suspendisse potenti. Vestibulum vel libero eu nulla varius pretium eget eu magna. Pellentesque habitant morbi tristique senectus et netus et malesuada...",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit amet tempor magna. Donec eros sem, porta eget leo et, varius eleifend mauris. Donec eu leo congue, faucibus quam eu, viverra mauris. Nulla consectetur lorem mi, at scelerisque metus hendrerit vitae. Proin vel magna vel neque porta ultricies non eget mauris. Suspendisse potenti. Aliquam faucibus scelerisque placerat. Vestibulum vel libero eu nulla varius pretium eget eu magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean dictum faucibus felis, ac vestibulum risus mollis in. Phasellus neque dolor, euismod vitae auctor eget, dignissim a felis. Etiam malesuada elit a nibh aliquam, placerat ultricies nibh dictum. Nam ut egestas velit. Pellentesque viverra tincidunt tellus. Etiam cursus, ligula id vehicula cursus, turpis mauris facilisis massa, eget tincidunt est purus et odio. Nam quis luctus libero, non posuere velit. Ut eu varius diam, eu euismod elit. Donec efficitur, neque eu consectetur consectetur, dui sem consectetur felis, vitae rutrum risus urna vel arcu. Aliquam semper ullamcorper laoreet. Sed arcu lectus, fermentum imperdiet purus eu, ornare ornare libero",
   },
   {
     id: 3,
@@ -29,6 +29,6 @@ export const posts = [
     categories: ["fashion", "winter"],
     comments: 2,
     image: images.blog03,
-    excerpt: "Etiam cursus, ligula id vehicula cursus, turpis mauris facilisis massa, eget tincidunt est purus et odio. Nam quis luctus libero, non posuere velit...",
+    excerpt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sed turpis sed lorem dignissim vulputate nec cursus ante. Nunc sit amet tempor magna. Donec eros sem, porta eget leo et, varius eleifend mauris. Donec eu leo congue, faucibus quam eu, viverra mauris. Nulla consectetur lorem mi, at scelerisque metus hendrerit vitae. Proin vel magna vel neque porta ultricies non eget mauris. Suspendisse potenti. Aliquam faucibus scelerisque placerat. Vestibulum vel libero eu nulla varius pretium eget eu magna. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean dictum faucibus felis, ac vestibulum risus mollis in. Phasellus neque dolor, euismod vitae auctor eget, dignissim a felis. Etiam malesuada elit a nibh aliquam, placerat ultricies nibh dictum. Nam ut egestas velit. Pellentesque viverra tincidunt tellus. Etiam cursus, ligula id vehicula cursus, turpis mauris facilisis massa, eget tincidunt est purus et odio. Nam quis luctus libero, non posuere velit. Ut eu varius diam, eu euismod elit. Donec efficitur, neque eu consectetur consectetur, dui sem consectetur felis, vitae rutrum risus urna vel arcu. Aliquam semper ullamcorper laoreet. Sed arcu lectus, fermentum imperdiet purus eu, ornare ornare libero",
   }
 ];
--- src/pages/home/partials/Blog.jsx
+++ src/pages/home/partials/Blog.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { posts } from '../../../json/posts';
+import images from '../../../constants/images';
 
 const Blog = () => {
     return (
-        <div>
-            
-            <div>
-                
+        <div className='lg:p-10 p-5'>
+            <div className='items-center justify-center lg:flex gap-10'>
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        {post.image && (
+                             <img 
+                             src={post.image}
+                             className='w-full h-70 object-cover' alt="" />
+                        )}
+                        <div className='py-3 space-y-3'>
+                        <h2 className='font-semibold text-md text-black/80'>{post.title}</h2>
+                        <p className='text-black/45 ' >by <span className='text-black/60'>{post.author}</span> on <span className='text-black/60'>{post.date}</span> </p>
+                        <p className='text-black/35'>{post.excerpt.slice(0,200)}...</p>
+                        </div>
+                    </div>
+                ))}
+
+
             </div>
         </div>
     );
